739aec841f5f89a9bd2faddc521adfc32b284494
Rainbow: the Mad Faerie <rtmf@beautifulsunrise.org>
2015-06-14 09:39:31 -0700

    work toward getting interfaces set up properly for
    hostapd and friends to handle
    
    will be using a private ipv6 space for this since
    nothing else seems feasible.  this took much more
    time than it should have because as I finally did
    figure out, I was chasing a bug that actually was
    present in the config on horizon as it related to
    the link out to sunrise, but that was causing the
    route-v6 from kali to ping-pong or just blackhole
    when connecting from an otherwise sane config >.<
    
    chasing my tail(1)...

--- kali/fetch.sh
+++ kali/fetch.sh
@@ -1,3 +1,3 @@
-rsync -avP kali:/etc/strongswan.d/charon.conf kali:/etc/ipsec.conf kali:/etc/netctl/interfaces/usb0 kali:/etc/hosts kali:/etc/resolv.conf kali:/usr/local/bin/ap_updown kali:/usr/local/bin/mk_ap kali:/usr/local/bin/setup_dummy kali:/usr/local/bin/teardown_dummy kali:/root/nyghts.conf ./
+rsync -avP kali:/etc/strongswan.d/charon.conf kali:/etc/ipsec.conf kali:/etc/netctl/krystal kali:/etc/hosts kali:/etc/resolv.conf kali:/usr/local/bin/ap_updown kali:/usr/local/bin/mk_ap kali:/usr/local/bin/setup_dolly kali:/usr/local/bin/teardown_dolly kali:/root/nyghts.conf kali:/etc/sysctl-dolly.conf kali:/etc/netctl/dolly kali:/etc/netctl/intwifi.b43-CMnet2 kali:/etc/netctl/intwifi.wl0-CMnet2 kali:/etc/iftab kali:/etc/netctl/corset ./
 ssh kali ip route > route
 ssh kali ip addr > addr
